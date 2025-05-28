39482e46c4987709f36697f499cbf721488f2d48
Sarah Santos <sarah.santos5_estuda@C110730.pdc-academico.ifms.tl>
2025-05-27 21:55:15 -0400

    funções

---
+++ aula8/aula.py
@@ -0,0 +1,11 @@
+def subtracao(num1, num2):
+    print(num1 - num2)
+subtracao(2, 8) 
+
+def multiplicacao(num1, num2):
+    print(num1 * num2)
+multiplicacao(2, 8)
+
+def divisao(num1, num2):
+    print(num1 / num2)
+divisao(2, 8)
