3ca897729cbde592d215b687efbf18e3167d10ac
saaberbel <sarah.santos5@estudante.ifms.edu.br>
2025-06-03 22:02:40 -0400

    Lambda Function

--- aula.py
+++ aula.py
@@ -1,26 +1,34 @@
-def menu_cadastro():
-    print('=============== MENU ================')
-    print('(1)Cadastrar')
-    print('(2)Consultar')
-    print('(3) - Matricular')
-    print('(4) - Solicitar Histórico ')
-    print('(0) - Sair')
+'''
+Funções Lambda - Lambda Function
 
-def menu_consulta():
-    print('=============== MENU ================')
-    print('(1) - Aluno')
-    print('(2) - Professorr')
-    print('(0) - Sair')
+def nome(x, y, z)
+    corpo da função
     
-op = input('Digite uma opção:')
+sintaxe: lambda argumentos: expressão
+- não existem para que se crie funçoes de muitas linhas, são funlções expressas.
 
-# Validar se foi digitado um numero ? (if)
-op = int(op)
+'''
+#dobro de uma variável
 
-if op >= 0 and op <= 0:
-    if op == 1:
-        menu_cadastro()
-    elif op == 2:
-        menu_consulta()
-else : 
-    print('Erro na escolha das opções!')
+dobro = lambda x: x * 2
+
+print('Exemplo - 1: Dobro de 5 = ', dobro(5))
+
+soma = lambda x, y: x + y
+print('Exemplo - 2: Soma de 3 e 4 = ', soma(3, 4))
+
+
+#hipotenusa de um triangulo
+'''catop ** 2 + catadj ** 2 == hipotenusa'''
+
+hipotenusa = lambda catop, catadj: (catop ** 2 + catadj ** 2) ** 0.5
+print('Exemplo - 3: hipotenusa = ', hipotenusa(8, 6))
+
+#filter, map
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+'''pares = filter(função, array)'''
+pares = list(filter(lambda x: x % 2 == 0, numeros))
+print('Exemplo - 4: Numeros Pares: ', pares(3, 4))
+
